c176b409382a228bcd909a1599f023a54a9c1b96
asalnassiri <31486750+asalnassiri@users.noreply.github.com>
2017-09-11 13:59:15 -0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -113,6 +113,13 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     },
     "retina_detect": true
+        "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
   });
 
 }, false);
